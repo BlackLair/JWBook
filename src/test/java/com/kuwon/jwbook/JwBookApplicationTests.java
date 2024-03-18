d53730732cd21c783a6c80f246417b945da0fb0b
JW <kiw970923@naver.com>
2024-03-18 17:39:32 +0900

    [ADD] JWBook 프로젝트 생성

---
+++ src/test/java/com/kuwon/jwbook/JwBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuwon.jwbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
