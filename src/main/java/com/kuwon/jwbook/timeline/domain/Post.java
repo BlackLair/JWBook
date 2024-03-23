00dc25de09880b2a0baa8d772c0206c6fd188df8
JW <kiw970923@naver.com>
2024-03-23 15:47:36 +0900

    [feat] 게시글, 댓글 Entity 클래스 설계
     - Post 클래스에 게시글 작성자의 userIdStr, 좋아요 개수를 나타내는 likeCount 필드 추가

---
+++ src/main/java/com/kuwon/jwbook/timeline/domain/Post.java
@@ -0,0 +1,66 @@
+package com.kuwon.jwbook.timeline.domain;
+
+import java.util.Date;
+
+public class Post {
+	private int id;
+	private int userId;
+	private String contents;
+	private String imagePath;
+	private Date createdAt;
+	private Date updatedAt;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	private String userIdStr;
+	private int likeCount;
+
+	public String getUserIdStr() {
+		return userIdStr;
+	}
+	public void setUserIdStr(String userIdStr) {
+		this.userIdStr = userIdStr;
+	}
+	public int getLikeCount() {
+		return likeCount;
+	}
+	public void setLikeCount(int likeCount) {
+		this.likeCount = likeCount;
+	}
+	
+}
---
+++ src/main/java/com/kuwon/jwbook/timeline/domain/Reply.java
@@ -0,0 +1,48 @@
+package com.kuwon.jwbook.timeline.domain;
+
+import java.util.Date;
+
+public class Reply {
+	private int id;
+	private int postId;
+	private int userId;
+	private String contents;
+	private Date createdAt;
+	private Date updatedAt;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPostId() {
+		return postId;
+	}
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
